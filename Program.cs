f52b9d184387cb2ce53742f62ac4e78c1e46cdee
EnanderW <williamenander@gmail.com>
2024-09-12 10:42:39 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace lektion9;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var first = Environment.GetEnvironmentVariable("FIRST");
+        var second = Environment.GetEnvironmentVariable("SECOND_FILE");
+        Console.WriteLine(first);
+        Console.WriteLine(second);
+
+        var secondValue = File.ReadAllText(second);
+        Console.WriteLine(secondValue);
+    }
+}
